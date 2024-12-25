74fcb39e7efccab509f9e9cd18b558e67b560677
Abhilash Kaluwala <85585567+abhilashkaluwala@users.noreply.github.com>
2024-12-25 01:58:00 -0500

    Delete app directory

--- app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, request, redirect, url_for
-from app import app
-
-@app.route('/')
-def home():
-    return "Welcome to the Diary App"
-
-# Add more routes as we progress
